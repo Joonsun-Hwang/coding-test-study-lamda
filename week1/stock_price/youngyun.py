55a38cda0941f0764f863365bf9f0d708a064114
fightnyy <fightnyy@naver.com>
2021-01-15 01:42:57 +0900

    week 1 youngyun

---
+++ week1/stock_price/youngyun.py
@@ -0,0 +1,14 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)) :
+        answer.append(0)
+                
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            answer[i]+=1
+            if prices[i] <= prices[j]:
+                pass
+            else :
+                break
+                
+    return answer
