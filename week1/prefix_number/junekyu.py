67f45a3ad9d92f13cf7e2800f2c5c6ef07e99356
JuneKyu Park <IDbluefish@gmail.com>
2021-01-16 00:43:36 +0900

    Add files via upload

---
+++ week1/prefix_number/junekyu.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pdb
+import copy
+
+def solution(phone_book):
+    flag = True
+    for p in phone_book:
+        temp_phone_book = list(phone_book)
+        temp_phone_book.remove(p)
+        for q in temp_phone_book:
+            if q.split(p)[0] == '':
+                return False
+    return True
+
+
+def main():
+    inputs = ['119', '97674223', '1195524421']
+    answer = solution(inputs)
+    print(answer)
+    inputs = ['123','456','789']
+    answer = solution(inputs)
+    print(answer)
+    inputs = ['12','123','1235','567','88']
+    answer = solution(inputs)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
