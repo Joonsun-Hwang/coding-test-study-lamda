e11bdb27f52c22019432e4192c2c37ee487c0e4a
fightnyy <fightnyy@naver.com>
2021-03-11 16:15:23 +0900

    youngyun.py

---
+++ week9/exam_supervisor/youngyun.py
@@ -0,0 +1,19 @@
+# 시험장 갯수
+n = int(input())
+n_exams=list(map(int,input().split()))
+main, support = list(map(int,input().split()))
+answer = 0
+for idx in range(len(n_exams)):
+    n_exams[idx] -= main
+    if n_exams[idx]<=0:
+        n_exams[idx] = 0
+    answer += 1
+    
+    num = n_exams[idx] // support
+    if n_exams[idx] % support != 0:
+        num += 1
+    answer += num
+
+print(answer)
+        
+    
