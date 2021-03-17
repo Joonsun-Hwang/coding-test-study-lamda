5cb00bd37372e6da8e8184a6c3a7b293aa0c9d40
fightnyy <fightnyy@naver.com>
2021-03-17 23:41:26 +0900

    youngyun.py

--- week10/complex/youngyun.py
+++ week10/complex/youngyun.py
@@ -9,27 +9,27 @@ def main():
     many = 0
     answer = []
     for _ in range(num):
-        complex.append(list(map(int,input().strip("\n"))))
+        complex.append(list(map(int, input().strip("\n"))))
+
     def _dfs(i, j):
-        
-        if i >= len(complex) or j >= len(complex) or i < 0 or j<0:
-            return 
+
+        if i >= len(complex) or j >= len(complex) or i < 0 or j < 0:
+            return
         if complex[i][j] == 0:
             return
         else:
-            complex[i][j]=0
+            complex[i][j] = 0
             nonlocal live
             live += 1
-            _dfs(i,j+1)#우
-            _dfs(i,j-1)#좌
-            _dfs(i+1,j)#상
-            _dfs(i-1,j)#하
-    
-    
+            _dfs(i, j + 1)  # 우
+            _dfs(i, j - 1)  # 좌
+            _dfs(i + 1, j)  # 상
+            _dfs(i - 1, j)  # 하
+
     for i in range(len(complex)):
         for j in range(len(complex[0])):
             if complex[i][j] == 1:
-                _dfs(i,j)
+                _dfs(i, j)
                 answer.append(live)
                 many += 1
                 live = 0
@@ -38,8 +38,6 @@ def main():
     answer.sort()
     for ans in answer:
         print(ans)
-
-
 
 
 if __name__ == "__main__":
