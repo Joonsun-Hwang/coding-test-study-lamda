d3dc3de3fc8b2cefe8db37a80a0e5e7538417e8f
fightnyy <fightnyy@naver.com>
2021-03-18 22:20:40 +0900

    youngyun.py

--- week10/treasure_island/youngyun.py
+++ week10/treasure_island/youngyun.py
@@ -1,33 +1,48 @@
 import sys
-from defaultdict import deque
-from copy import deepcopy
-
-"""
-BFS
-Floyd-warshall?
-
-"""
-
-def BFS(i,j,count,visited):
-    q=deque()
-    if
+from collections import deque
 
 
-def main():
-    visited = deepcopy(riddle)
-    for i in range(height):
-        for j in range(width):
-            if riddle[i][j] == "W":
-                pass
-            else:
-                BFS(i,j,0,visited)
+def main(i, j, cnt):
+
+    dx = [-1, 0, 1, 0]
+    dy = [0, -1, 0, 1]
+    q = deque()
+    q.append((i, j, cnt))
+    c = [[0] * col for _ in range(row)]
+    while q:
+        i, j, cnt = q.popleft()
+        c[i][j] = 1
+        for derivate in range(4):  # 좌하우상
+            if (
+                i + dx[derivate] >= 0
+                and j + dy[derivate] >= 0
+                and i + dx[derivate] < row
+                and j + dy[derivate] < col
+                and riddle[i + dx[derivate]][j + dy[derivate]] == "L"
+                and c[i + dx[derivate]][j + dy[derivate]] == 0
+            ):
+                c[i + dx[derivate]][j + dy[derivate]] = 1
+                q.append(
+                    (
+                        i + dx[derivate],
+                        j+ dy[derivate],
+                        cnt + 1,
+                    )
+                )
+
+    return cnt
 
 
 if __name__ == "__main__":
     input = sys.stdin.readline
-    height, width = list(map(int,input().strip("\n").split()))
+    row, col = map(int, input().strip("\n").split())
     riddle = []
-    for _ in range(height):
-        riddle.append(input().strip("\n").split())
-    main()
-    visited = None
+    cnt = -1
+    for _ in range(row):
+        tmp = list(input().strip("\n"))
+        riddle.append(tmp)
+    for i in range(row):
+        for j in range(col):
+            if riddle[i][j] == "L":
+                cnt = max(cnt, main(i, j, 0))
+    print(cnt)
